ca1be2b988f12207b5f992bd5d31c72292df1afe
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-04 18:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = []
   for (let i = 0; i < 5; i++){
-    array.push(`${musicians[0]} plays ${instruments[0]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 return array
 }
