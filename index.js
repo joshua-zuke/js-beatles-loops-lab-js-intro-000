972e1307e47ab3d8add3d0a2063d0090dbae3c1b
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-04 18:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-
+  for (let i = 0; i < 5; i++){
+    array.push(`${musicians[0]} plays ${instruments[0]}`)
+  }
+return array
 }
+
+// needs to take each element from both arrays
+// loop with a counter, i= 0, some condition, i ++1
+// `${musicians[0]} plays ${instruments[0]}`
+// need to add to array, I think .push is okay because we only need one array
+// return
